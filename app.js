030e68bd3a6783c552792ca1983b160639c83529
smarttin <smarttin77@gmail.com>
2020-09-18 18:04:38 +0100

    views setup

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ import userRouter from './router/userRouter';
 import videoRouter from './router/videoRouter';
 import globalRouter from './router/globalRouter';
 import routes from './routes';
+import { localsMiddleware } from './middlewares';
 
 const app = express();
 
+app.use(helmet());
+app.set('view engine', 'pug');
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
-app.use(helmet());
+app.use(localsMiddleware);
+
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- init.js
+++ init.js
@@ -1,5 +1,5 @@
 import app from './app';
-const PORT = 6000;
+const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
